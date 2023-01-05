215d03ed489211bae208caa657503bd7237b4119
robot12580 <ruijiesze@foxmail.com>
2023-01-05 16:49:32 +0800

    feat: build

--- es/lib/index.d.ts
+++ es/lib/index.d.ts
@@ -1,3 +1,4 @@
 import { useLoading } from './useLoading/index';
 import { useRestHeight } from './useRestHeight/index';
-export { useLoading, useRestHeight };
+import { useBus } from './useBus/index';
+export { useLoading, useRestHeight, useBus };
