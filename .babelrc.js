87e64e7a2f57e3ab3dab2a46c4a401e75f7e3feb
robot12580 <ruijiesze@foxmail.com>
2023-01-06 16:57:38 +0800

    build: babel presets

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,9 @@ module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-typescript',
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
+    [
+      '@babel/preset-react',
+      { runtime: 'automatic' }
+    ]
+  ]
 };
