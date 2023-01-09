f8b6c67c9786c764aa71213674f6d5c4622130d6
robot12580 <ruijiesze@foxmail.com>
2023-01-09 13:33:13 +0800

    build: separate .d.ts file from esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,37 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-export default {
-  input: "src/index.ts",
-  output: {
-    file:'es/index.js',
-    format:'es'
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json';
+
+module.exports = [
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.main,
+      format: 'es'
+    },
+    external: ['react', 'lodash'],
+    plugins: [
+      nodeResolve(),
+      typescript({
+        compilerOptions: {
+          declaration: false
+        }
+      }),
+      commonjs(),
+      terser()
+    ]
   },
-  external: ['react', 'lodash'],
-  plugins: [
-    nodeResolve(),
-    typescript(),
-    commonjs()
-  ]
-}
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.types,
+      format: 'es'
+    },
+    external: ['react', 'lodash'],
+    plugins: [
+      typescript()
+    ]
+  }
+]
