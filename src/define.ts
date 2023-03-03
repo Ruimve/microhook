ad6ad3b74b8ccba05abb91563997ebdf6327437c
robot12580 <ruijiesze@foxmail.com>
2023-03-03 15:44:56 +0800

    feat: add ts tools ReturnTypeWithPromise<T>

--- src/define.ts
+++ src/define.ts
@@ -1,1 +1,3 @@
 export type ReturnValue<T, P extends Object> = [T, P];
+
+export type ReturnTypeWithPromise<T extends (...args: any[]) => Promise<any>> = T extends (...args: any[]) => Promise<infer R> ? R : any;
