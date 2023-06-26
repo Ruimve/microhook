3d342beb9a2fb80130f6a81c7e8e0b66fcadda03
robot12580 <ruijiesze@foxmail.com>
2023-06-26 16:13:15 +0800

    fix: return parameters and catch error

--- src/useLoading/__tests__/index.spec.tsx
+++ src/useLoading/__tests__/index.spec.tsx
@@ -59,6 +59,44 @@ describe('Test useLoading', () => {
       await result.current[1].wrapRequset();
     });
 
-    expect(result.current[0].data).toEqual(error);
+    expect(result.current[0].data).toEqual(null);
   });
+
+
+  it('second times if error return prvious value', async () => {
+    const people = [{ name: 'Xiao Ming', age: 18 }];
+    const error = { message: 'exception caught' };
+    let count = 0;
+    const callback = jest.fn();
+    callback.mockImplementation(() => {
+      if(count === 0){
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            count = 1
+            resolve(people);
+          }, 1000);
+        });
+      }else{
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            reject(error);
+          }, 1000);
+        });
+      }
+    });
+    const { result } = renderHook(() => useLoading<typeof callback>(callback));
+    await act(async () => {
+      await result.current[1].wrapRequset();
+    });
+    expect(result.current[0].data).toEqual(people);
+
+    /** second times if error return prvious value **/
+    await act(async () => {
+      await result.current[1].wrapRequset();
+    });
+    
+    expect(result.current[0].data).toEqual(people);
+
+  });
+  
 });
--- src/useLoading/index.tsx
+++ src/useLoading/index.tsx
@@ -3,53 +3,55 @@
  * @description Handles the pending status of API or Promise.
  */
 
- import { useState, useCallback } from "react";
- import { ReturnTypeWithPromise, ReturnValue } from '../define';
- 
- type IFunction = (...args: any[]) => Promise<any>;
- type IArray = any[];
- 
- interface Response<T> {
-   loading: boolean; // Whether the request is loading
-   data: T | null; // The data returned by the request
- }
- 
- interface Action<T extends IArray> {
-   wrapRequset: (...args: T) => Promise<void>; // A function that wraps the request and handles loading state
- }
- 
- function createResponse<T>(loading: boolean, data: T | null = null): Response<T> {
-   return {
-     loading,
-     data
-   }
- }
- 
- function useLoading<
-   T extends IFunction,
-   P extends IArray = Parameters<T>,
-   U extends ReturnTypeWithPromise<T> | Error = ReturnTypeWithPromise<T>
- >(request: IFunction): ReturnValue<Response<U>, Action<P>> {
-   // Use React hook to manage state
-   const [response, setResponse] = useState<Response<U>>({ loading: false, data: null });
- 
-   const wrapRequset = useCallback(
-     async (...args: P) => {
-       setResponse(createResponse<U>(true)); // Set loading state to true
-       try {
-         const data = await request(...args); // Execute request
-         setResponse(createResponse<U>(false, data)); // Set loading state to false and store the response data
-       } catch (e) {
-         setResponse(createResponse<U>(false, e as U)); // If an error occurs, set loading state to false and store the error
-       }
-     },
-     [request]
-   )
- 
-   return [response, { wrapRequset }]
- }
- 
- export {
-   useLoading
- }
- 
+import { useState, useCallback } from "react";
+import { ReturnTypeWithPromise, ReturnValue } from '../define';
+
+type IFunction = (...args: any[]) => Promise<any>;
+type IArray = any[];
+type IReturn = any;
+
+interface Response<T> {
+  loading: boolean; // Whether the request is loading
+  data: T | null; // The data returned by the request
+}
+
+interface Action<T extends IArray, U> {
+  wrapRequset: (...args: T) => Promise<U>; // A function that wraps the request and handles loading state
+}
+
+function createResponse<T>(loading: boolean, data: T | null = null): Response<T> {
+  return {
+    loading,
+    data
+  }
+}
+
+function useLoading<
+  T extends IFunction,
+  P extends IArray = Parameters<T>,
+  U extends IReturn = ReturnTypeWithPromise<T>
+>(request: IFunction): ReturnValue<Response<U>, Action<P, U>> {
+  // Use React hook to manage state
+  const [response, setResponse] = useState<Response<U>>({ loading: false, data: null });
+
+  const wrapRequset = useCallback(
+    async (...args: P) => {
+      try {
+        setResponse(createResponse<U>(true, response.data)); // Set loading state to true
+        const data = await request(...args); // Execute request
+        setResponse(createResponse<U>(false, data)); // Set loading state to false and store the response data
+        return data;
+      } catch (e) {
+        setResponse(createResponse<U>(false, response.data));
+        return response.data;
+      }
+    },
+    [request, response]
+  )
+
+  return [response, { wrapRequset }]
+}
+
+export {
+  useLoading
+}
